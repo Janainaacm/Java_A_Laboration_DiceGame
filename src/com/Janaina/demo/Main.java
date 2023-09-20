922889d25098d96a83c848828df1cf202bbd20d5
janainaacm <janaina@Janainas-Air.lan>
2023-09-20 14:34:03 +0200

    Second commit

--- src/com/Janaina/demo/Main.java
+++ src/com/Janaina/demo/Main.java
@@ -1,2 +1,66 @@
-package com.Janaina.demo;public class Main {
+package com.Janaina.demo;
+
+import com.Janaina.demo.templates.Players;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("\u001B[35m" + "WELCOME TO THE DICE GAME!" + "\u001B[0m");
+
+        selectPlayers();
+
+
+
+
+
+    }
+
+
+    public static void selectPlayers(){
+        System.out.println("\u001B[36m" + "Firstly, how many players will be joining us?" + "\u001B[0m");
+        int numberOfPlayers = scannerNumber();
+
+        String[] arrayPlayers = new String[numberOfPlayers];
+
+        System.out.println("\u001B[35m" + "Amazing!" + "\u001B[0m");
+        System.out.println("\u001B[36m" + "Enter the players names: " + "\u001B[0m");
+        for(int i=0; i<numberOfPlayers; i++) {
+
+            arrayPlayers[i]= scannerText();
+        }
+        System.out.println("Player names are: ");
+
+        for (int i=0; i<numberOfPlayers; i++){
+            System.out.println(arrayPlayers[i]);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+    public static int scannerNumber(){
+        Scanner sc = new Scanner(System.in);
+
+        return sc.nextInt();
+
+    }
+
+
+    public static String scannerText(){
+        Scanner sc = new Scanner(System.in);
+
+        return sc.nextLine();
+
+    }
+
+
 }
--- src/com/Janaina/demo/templates/Players.java
+++ src/com/Janaina/demo/templates/Players.java
@@ -1,2 +1,30 @@
-package com.Janaina.demo.templates;public class Players {
+package com.Janaina.demo.templates;
+
+public class Players {
+
+    public String name;
+    private int numberOfPlayers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+for (int i = 0; i < numberOfPlayers; i++) {
+System.out.println("Player " + (i+1) + ", what's your name?");
+            String playerName = scannerText();
+            Players player = new Players(playerName);
+            }
+ */
+
 }
+
+
