e77356ee5b789c83b55f6f46ff9c9e8064c40982
janainaacm <janaina@Janainas-Air.lan>
2023-09-21 16:05:12 +0200

    Third commit

--- src/com/Janaina/demo/Main.java
+++ src/com/Janaina/demo/Main.java
@@ -2,6 +2,8 @@ package com.Janaina.demo;
 
 import com.Janaina.demo.templates.Players;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -10,44 +12,86 @@ public class Main {
 
         System.out.println("\u001B[35m" + "WELCOME TO THE DICE GAME!" + "\u001B[0m");
 
-        selectPlayers();
-
-
-
-
-
-    }
-
-
-    public static void selectPlayers(){
         System.out.println("\u001B[36m" + "Firstly, how many players will be joining us?" + "\u001B[0m");
         int numberOfPlayers = scannerNumber();
 
-        String[] arrayPlayers = new String[numberOfPlayers];
+        Players[] arrayPlayers = new Players[numberOfPlayers];
 
         System.out.println("\u001B[35m" + "Amazing!" + "\u001B[0m");
+
+        System.out.println("\u001B[36m" + "How many dice do you want each player to throw? " + "\u001B[0m");
+        int numberOfDice = scannerNumber();
+
         System.out.println("\u001B[36m" + "Enter the players names: " + "\u001B[0m");
-        for(int i=0; i<numberOfPlayers; i++) {
 
-            arrayPlayers[i]= scannerText();
-        }
-        System.out.println("Player names are: ");
+        for (int i = 0; i < numberOfPlayers; i++) {
 
-        for (int i=0; i<numberOfPlayers; i++){
-            System.out.println(arrayPlayers[i]);
+            String playerNames = scannerText();
+            arrayPlayers[i] = new Players(playerNames, numberOfDice);
 
         }
+
+
+        System.out.println("\u001B[35m" + "NOW LET'S BEGIN!" + "\u001B[0m");
+        Random random = new Random();
+
+        for (int i = 0; i <= numberOfPlayers; i++) {
+            if (i == numberOfPlayers){
+                Players winner = null;
+
+                for (Players p : arrayPlayers) {
+                    winner = new Players(p.name, 0);
+                    while (winner.score < p.score) {
+                        winner = p;
+                    }
+                }
+                System.out.println("The winner of this round is: " + winner.name + winner.score);
+            }
+
+            boolean isPlaying = true;
+
+            do {
+
+                System.out.println("\u001B[36m" + arrayPlayers[i].name + "! Type 'go' to throw your dice, or 'stop' to quit game..." + "\u001B[0m");
+                String action = scannerText();
+
+                switch(action){
+                    case "stop":
+                        isPlaying = false;
+                        break;
+                    case "go":
+                        System.out.println("PLAYING!");
+
+                        arrayPlayers[i].score = random.nextInt(6 - 1 ) * numberOfDice;
+                        System.out.println("Your score is: " + arrayPlayers[i].score);
+
+                        isPlaying = false;
+                        break;
+                    default:
+                        System.out.println("Wrong input");
+                        break;
+                }
+
+            } while (isPlaying);
+
+        }
+
+
     }
 
 
+/*
+  System.out.println("Player names are: ");
+
+        for (int i = 0; i < numberOfPlayers; i++) {
+            System.out.println(arrayPlayers[i]);
+
+        }
 
 
+ */
 
-
-
-
-
-    public static int scannerNumber(){
+    public static int scannerNumber() {
         Scanner sc = new Scanner(System.in);
 
         return sc.nextInt();
@@ -55,7 +99,7 @@ public class Main {
     }
 
 
-    public static String scannerText(){
+    public static String scannerText() {
         Scanner sc = new Scanner(System.in);
 
         return sc.nextLine();
--- src/com/Janaina/demo/templates/Players.java
+++ src/com/Janaina/demo/templates/Players.java
@@ -3,7 +3,29 @@ package com.Janaina.demo.templates;
 public class Players {
 
     public String name;
-    private int numberOfPlayers;
+    public int score;
+
+    public Players(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore (){
+        return score;
+    }
+
+    public void setScore(){
+        this.score = score;
+    }
+
 
 
 
